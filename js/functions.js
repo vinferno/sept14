dfa9b2d47966bbeb27a751400e9d17c931ef3c1b
vinferno <vinsonfernandez@gmail.com>
2015-09-14 16:28:43 -0700

    threshold1

--- js/functions.js
+++ js/functions.js
@@ -54,6 +54,8 @@ var add_minus_listener = function(){
 var start_swipe = 0;
 var end_swipe = 0;
 var threshold = 100;
+var y_threshold = 10;
+
 var function_loop_mouse_enter = function(){
 	for (var i= 0;i<game_list_item.length;i++){
 
@@ -63,7 +65,7 @@ var function_loop_mouse_enter = function(){
 
 		game_list_item[i].addEventListener('mouseup', function(){
 			end_swipe = event.clientX;
-			if (start_swipe + threshold< end_swipe ){
+			if (start_swipe + threshold < end_swipe ){
 				alert("swipe right");				
 			}
 			if (start_swipe > end_swipe + threshold){
@@ -84,12 +86,26 @@ var function_loop_mouse_enter = function(){
 };
 	
 	game_list_item[3].addEventListener('touchstart', function(event){ 
+		y_threshold = y_threshold + game_list_item[3].innerHeight
 		start_swipe = event.touches[0].pageX;
+		start_swipe_y = event.touches[0].pageY;
 
 	});  
-	game_list_item[3].addEventListener('touchend', function(event){     	
-     			end_swipe = event.changedTouches[0].pageX;
-     			
+	game_list_item[3].addEventListener('touchend', function(event){ 
+
+     		end_swipe = event.changedTouches[0].pageX;
+     		end_swipe_y = event.changedTouches[0].pageY;
+
+     		if (start_swipe_y < end_swipe_y){
+     			if (end_swipe_y - start_swipe_y > y_threshold){
+     				return;
+     			}
+     		} 
+     		if (start_swipe_y > end_swipe_y){
+     			if (start_swipe_y - end_swipe_y > y_threshold){
+     				return;
+     			}
+     		}       			
 			if (start_swipe + threshold< end_swipe ){
 				alert("swipe right");				
 			}
@@ -97,4 +113,4 @@ var function_loop_mouse_enter = function(){
 				alert('swipe left')
 			}
 		});
-alert('newtouch9');
+alert('threshold1');
