0c1ead08a3f2f0e5cc0dde5e3bb041a3f6713f7a
vinferno <vinsonfernandez@gmail.com>
2015-09-14 10:28:45 -0700

    get click back

--- js/add_listeners.js
+++ js/add_listeners.js
@@ -5,11 +5,11 @@ game_unordered_list.onclick = function(event) {
     function_minus(target);   
 };
 
-// game_list_item[0].addEventListener('click',function(){
-// 	game_list_item[1].classList.toggle("closed");
-// 	game_list_item[1].classList.toggle("open");
-// 	//add_game();
-// });
+game_list_item[0].addEventListener('click',function(){
+	game_list_item[1].classList.toggle("closed");
+	game_list_item[1].classList.toggle("open");
+	//add_game();
+});
 
 add_submit.addEventListener('click',function(){
 	var name_value = document.getElementById("name_input_box").value;
